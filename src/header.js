16927b5de6199ff993f4ac88a40a779ae5ba1bde
Leslie Liang <leslie.liang@ngc.com>
2020-07-12 22:21:01 -0700

    feat: updated nav bar text shadows

--- src/header.js
+++ src/header.js
@@ -5,20 +5,20 @@ export const  loadHeader = () => {
   
   const markup=
    `<div class="header">
-      <h1 style="margin-left: 30px">Pazzo Pizzeria</h1>
-      <img id="pazzo" src='./img/pazzo.jpeg' alt="pazzo pizzeria">
-      <div style="margin-right: 30px" class="nav">
-        <a class="nav-link home">
-          <span class="text-shadow">Home</span>
-        </a>
-        <a class="nav-link menu">
-          Menu
-        </a>
-        <a class="nav-link contact">
-          Contact
-        </a>
+        <h1 style="margin-left: 30px">Pazzo Pizzeria</h1>
+        <img id="pazzo" src='./img/pazzo.jpeg' alt="pazzo pizzeria">
+        <div style="margin-right: 30px" class="nav">
+          <a class="nav-link home">
+            Home
+          </a>
+          <a class="nav-link menu">
+            Menu
+          </a>
+          <a class="nav-link contact">
+            Contact
+          </a>
+        </div>
       </div>
-    </div>
     `
 
   elements.content.insertAdjacentHTML('beforeend', markup)
